6af8e46f26082463846bd13b11a0e159f26a533f
abureau <abureau6@gmail.com>
2020-05-05 11:13:57 -0400

    Correction to the asymptotic variance with single matrix

--- R/contact_1mat.R
+++ R/contact_1mat.R
@@ -1,4 +1,4 @@
-contact.1mat = function(theta,alpha=0.05)
+contact.1mat = function(theta,Nj=1,alpha=0.05)
 #' @description Reconstruction of contact matrix and estimation of confidence bounds from coefficients when there is a single contact matrix
 #' @param theta Vector of parameters
 #' @param alpha Significance level
@@ -11,7 +11,7 @@ contact.1mat = function(theta,alpha=0.05)
 	mult = matrix(1/sqrt(2),nn,nn)
 	diag(mult) = 1
 # On construit les bornes de confiance sur l'échelle log
-	l = mult*qnorm(1-alpha/2)*sqrt(1/mu+1/a)
+	l = mult*qnorm(1-alpha/2)*sqrt((1/mu+1/a)/Nj)
 	lb = exp(b-l)
 	ub = exp(b+l)
 	list(estim=mu,lb=lb,ub=ub)
